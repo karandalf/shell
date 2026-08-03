030283396ecb38b45a8abcdd5bab302f7ee96452
gilax <gilax@user.localdomain>
2026-08-03 12:24:02 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@ int main(int argc, char *argv[]) {
 	setbuf(stdout, NULL);
 	char command[BUFSIZE];
 
+	while (true){
 	printf("$ ");
 	getInput(command);
 	printf("%s: command not found\n", command);
 
 	return 0;
+	}
 }
