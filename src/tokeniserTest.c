da65c9b14c8c9ef13f1ffbe8b25303304a11cc15
gilax <gilax@user.localdomain>
2026-08-08 18:17:48 +0100

    codecrafters submit [skip ci]

--- src/tokeniserTest.c
+++ src/tokeniserTest.c
@@ -90,7 +90,7 @@ void parseInput(struct shell *shell){
 		quote = c;	
 		i = 0;
 		while ((c = getchar()) != quote)
-			*(shell->comannd + i++);
+			*(shell->command + i++);
 		c = getchar();
 	}
 	else{
